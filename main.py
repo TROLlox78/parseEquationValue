41995b77567c1c71b545b6a974fdd9e9a093b623
rectangle_man <trollox.78@gmail.com>
2024-10-19 15:58:12 +0200

    more

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from openpyxl import load_workbook
-import sympy
 import pytexit
-workbook = load_workbook(filename="dupa.xlsx" )
-#workbook = load_workbook(filename="C:\\Users\\rectangle_man\\dev\\python\\parseEquationValue\\dupa.xlsx" )
+workbook = load_workbook(filename="aa.xlsx" )
 sheet = workbook.active
 
 
@@ -35,30 +33,46 @@ def extract_values(cell):
     # finds the equation and values in the cell and builds a string
     s = ''
     skip_chr = 0;
-
     cell_value = sheet[cell].value
-    
+
     if isinstance(cell_value,(int,float)) :
         return str(cell_value)
     for i in range(1,len(cell_value)):
         if skip_chr>0:
             skip_chr-=1;
-            #print("skip:"+cell_value[i])
             continue;
         newcell = find_cell(cell_value[i:])
         if len(newcell) >0:
-            print(newcell)
             s+= extract_values(newcell)
             skip_chr = len(newcell)-1
         else:
             s+=cell_value[i]
-            print(cell_value[i])
     return s
 
-def extract():
-    x =extract_values("A2")
-    print(x)
-    #y = sympy.sympify(x)
-    #pytexit.py2tex(x)
-    #print(sympy.latex(y))
-extract()
+def shorten(s:str):
+    i = 0
+    for ch in s:
+        i+=1
+        if ch==".":
+            i+=2
+            return s[:i]
+        
+
+def extract(cells):
+    for cell in cells:
+        equation =extract_values(cell)
+        print(cell)
+        latex = pytexit.py2tex(equation, print_latex=False)
+        #print(latex + " = " + shorten(str(eval(equation))))
+
+
+
+cells = []
+
+for i in range(31,54):
+    if i==35 or i==36 or i ==47:
+        continue
+    cells.append(str('S'+str(i)))
+
+if __name__ == "__main__":
+    extract(cells)
