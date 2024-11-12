87ac006865be17aa2ec47a02a275d4f5b2308c1e
rectangle_man <trollox.78@gmail.com>
2024-11-12 14:11:15 +0100

    added rounding of floats

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import decimal
 from pickletools import read_stringnl_noescape_pair
 from openpyxl import load_workbook
 import pytexit
-filename = "lab_6.xlsx"
+import re
+
+# this import sys bad, hack for easy stdout redirect to file
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+filename = "aue_lab7.xlsx"
 workbook = load_workbook(filename=filename )
 workbook_data = load_workbook(filename=filename, data_only=True )
 sheet = workbook.active
@@ -36,12 +41,15 @@ def find_cell(s:str):
         i+=1
     return out
 
+# this is necessary
 from math import sqrt
 
 def clean_formula(val):
+    # replaces excel to python lang
     val= val.replace('^','**')
     val = val.replace("SQRT" , 'sqrt')
     val = val.replace("-0" , '-')
+    #val = val.replace("PI()" , '3.14')
     return val
 
 def extract_values(cell_value):
@@ -65,70 +73,65 @@ def extract_values(cell_value):
             s+=cell_value[i]
     return clean_formula(s)
 
-def shorten(s:str):
-    i = 0
-    for ch in s:
-        i+=1
-        if ch==".":
-            i+=2
-            return s[:i]
         
 
-def float_killer(s, round_to):
-    check_num = False;
-    decimal_point = False;
-    s = repr(s)
-
-    if round_to==0:
-        return s
-
-    new_s = ''
-    for ch in s:
-        if ch.isdigit() and not check_num:
-            check_num = True
-            decimal_point = False
-        if check_num:
-            if ch.isdigit() and not decimal_point:
-                new_s +=ch
-            if ch == '.':
-                count = 0
-                decimal_point = True
-                new_s+=ch
-            if decimal_point and ch.isdigit():
-                count+=1
-                if count<round_to:
-                    new_s +=ch
-            if not ch.isdigit() and ch != '.':
-                check_num = False;
-                new_s+=ch
-        else:
-            new_s +=ch
-    return new_s
-
 def extract(cell):
-    
+    # creates latex 
     equation =extract_values(cell)
     latex = pytexit.py2tex(equation, print_latex=False, print_formula=False)
     return latex
-        #print(latex + " = " + shorten(str(eval(equation))))
 
+def shorten_small_float(n : str, round_to):
+    # run for float<1
+    output = n[:2]
+    count =0
+    counting = False
+    # cut out "0."" wtih [2:]
+    for ch in n[2:]:
 
+        output+=ch
+        if int(ch)>0:
+            # found nonzero digit
+            counting=True
+        if counting:
+            count+=1
+        if count == round_to:
+            return output
+    return output
 
+def reduce_floats(s: str, round_to=2):
+    find_floats = r"\d+[.]\d+"
+    #teststr = repr("Tl1= = $$9.15\times{10}^{-12} \left(4600+292.7351842831629\times{10}^6\right)$$ = 0.0026785690261909405")
+
+    def matchfloat(matchobj):
+        round_to = 2
+        match = float(matchobj.group(0))
+        if match<1:
+            # so that you don't turn 0.004 => 0.0
+            return shorten_small_float(str(match), round_to)
+
+        return str(round(match, round_to))
+    
+    value_short = re.sub(find_floats, matchfloat ,s)
+    return value_short
 
 
 if __name__ == "__main__":
     cells = []
 
     # set cell search scope
-    start_cell = 12
-    end_cell   = 18
-    value_letter = 'w'
+    start_cell = 22
+    end_cell   = 40
+    value_letter = 'u'
 
 
     search_col = int(ord(value_letter))-int(ord('a'))+1
     for i in range(start_cell,end_cell):
-        name = sheet.cell(row=i, column=search_col-1).value
+        cell_name = sheet.cell(row=i, column=search_col-1).value
         #print(sheet.cell(row=i, column=search_col).value[1:])
         latex = extract(sheet.cell(row=i, column=search_col).value)
-        value = float_killer(datasheet.cell(row=i, column=search_col).value,0)
-        print(f'{name} = {latex} = {value}')
+        value = datasheet.cell(row=i, column=search_col).value
+
+        output = reduce_floats(f'{cell_name} = {latex} = {value}',2)
+        print(f'{cell_name} = {latex} = {value}')
+        print(output)
